3022caa179740a98dd3c9a8d126b3c8d9ea3c532
krysor <krysor901@gmail.com>
2022-12-02 14:23:50 +0100

    ft_atolst split in extra 2 functions

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 static t_list	**ft_parse(int argc, char *argv[]);
 static t_list	**ft_atolst(char **arr, int	argc);
+void			ft_atolst_exit(char **arr, int	argc, t_list **lst);
+int				ft_atolst_iserr(t_list **lst);
 static void		ft_freearr(char **arr);
 static void		ft_putarr_fd(char **arr, int fd);
 static void		ft_putlst_fd(t_list **lst, int fd);
@@ -69,8 +71,47 @@ static t_list	**ft_parse(int argc, char *argv[])
 }
 
 //THIS ONE SHOULD PROTECT MALLOC
+static t_list	**ft_atolst(char **arr, int	argc)
+{
+	int			i;
+	t_list		**lst;
 
-//consider splitting  the second if condition in while loop AND the free piece in 2 extra functions
+	i = 0;
+	lst = (t_list **)malloc(sizeof(t_list *));
+	if (lst != NULL)
+		*lst = NULL;
+	while (arr[i] != NULL)
+	{	
+		if (lst != NULL)
+			ft_lstadd_back(lst, ft_lstnew(ft_atopi(arr[i])));//!!!!is pi freed if atopi fails?!!!!!
+		if (ft_atolst_iserr(lst))
+			ft_atolst_exit(arr, argc, lst);
+		i++;
+	}
+	return (lst);
+}
+
+int	ft_atolst_iserr(t_list **lst)
+{
+	if (lst == NULL || ft_lstlast(*lst) == NULL
+		||ft_lstlast(*lst)->content == NULL || ft_isdup(lst))
+		return (1);
+	return (0);
+}
+
+void ft_atolst_exit(char **arr, int	argc, t_list **lst)
+{
+	if (argc == 2)
+		ft_freearr(arr);
+	ft_lstclear(lst, (void *)free);
+	if (lst != NULL)
+		free(lst);
+	ft_putstr_fd("Error\n", 2);
+	exit(-1);
+}
+
+/*
+//this one splits second while if statement and the error in 2 extra functions
 static t_list	**ft_atolst(char **arr, int	argc)
 {
 	int			i;
@@ -84,19 +125,29 @@ static t_list	**ft_atolst(char **arr, int	argc)
 	{	
 		if (lst != NULL)//protection requried here
 			ft_lstadd_back(lst, ft_lstnew(ft_atopi(arr[i])));//!!!!is pi freed if atopi fails?!!!!!
-		if (lst == NULL || ft_lstlast(*lst) == NULL ||//protection requried here
-			ft_lstlast(*lst)->content == NULL || ft_isdup(lst))
-		{
-			if (argc == 2)
-				ft_freearr(arr);
-			ft_lstclear(lst, (void *)free);
-			ft_putstr_fd("Error\n", 2);
-			exit(-1);
-		}
+		if (ft_atolst_iserr(lst))//protection requried here
+			ft_atolst_exit(arr, argc, lst);
 		i++;
 	}
 	return (lst);
 }
+
+int	ft_atolst_iserr(t_list **lst)
+{
+	if (lst == NULL || ft_lstlast(*lst) == NULL
+		||ft_lstlast(*lst)->content == NULL || ft_isdup(lst))//protection requried here
+		return (1);
+	return (0);
+}
+
+void ft_atolst_exit(char **arr, int	argc, t_list **lst)
+{
+	if (argc == 2)
+		ft_freearr(arr);
+	ft_lstclear(lst, (void *)free);
+	ft_putstr_fd("Error\n", 2);
+	exit(-1);
+}*/
 
 /*
 //THIS ONE DOES NOT USE NODE VARIABLE
