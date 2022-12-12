bd5c81ea9b477436d0e52929bbcb12872888001d
krysor <krysor901@gmail.com>
2022-12-12 16:29:28 +0100

    did some function splitting right before LIS

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 char	*ft_solve(t_list **input);
 int		ft_issorted(t_list **lst);
 
+void 	*ft_contdup(void *content);
+t_list 	**ft_lstdup(t_list **lst);
+//DELETE AT THE END:
+static void		ft_putlst_fd(t_list **lst, int fd);
+static void		ft_putcontent_fd(void *content);
+
 int main(int argc, char *argv[])
 {
     t_list	**input;
@@ -31,8 +37,48 @@ char	*ft_solve(t_list **a)
 		return ("sorted\n");	
 	//else, do the magic
 
+	//this makes copy of a with second a after first a
+	t_list **clone1 = ft_lstdup(a);
+	t_list **clone2 = ft_lstdup(a);
+	ft_lstlast(*clone1)->next = *clone2;
+	free(clone2);
+	printf("printing clone1 after elongation: \n");
+	ft_putlst_fd(clone1, 1);
+	ft_lstclear(clone1, (void *)free);
+	free(clone1);
+
+	//do the LIS stuff
+
 	//I'll need F returning LIS with its contents
 	return ("unsorted\n");
+}
+
+t_list **ft_lst_double(t_list **lst)
+{
+	t_list	**lst1;
+	t_list	**lst2;
+
+	if (lst == NULL)
+		return (NULL);
+	{
+		free(lst);
+		ft_putstr_fd("Error\n", 2);
+		exit(-1);
+	}
+	lst1 = ft_lstdup(lst);
+	lst2 = ft_lstdup(lst);
+	lst1 = ft_lstcat(lst1, lst2);
+	//naaaaaah
+	return ();
+}
+
+t_list **ft_lstcat(t_list **lst1, t_list **lst2)
+{
+	if (lst1 == NULL || lst2 == NULL)
+		return (NULL);
+	ft_lstlast(*lst1)->next = *lst2;
+	free(lst2);
+	return (lst1);
 }
 
 int	ft_issorted(t_list **lst)
@@ -61,7 +107,7 @@ step 2: pick the IS that's the longest
 		iterate over LIST with all IS and store the one that's currently the longest
 step 3: if more than 1, pick the first one
 */
-
+/*
 t_list **ft_lis(t_list **lst)
 {
 
@@ -76,4 +122,57 @@ t_list ***ft_is(t_list **lst)
 {
 	//this one should try each element after the current element as the next one AND all the elements before it
 
+}
+*/
+
+
+t_list **ft_lstdup(t_list **lst)
+{
+	t_list	**dup;
+	t_list	*temp;
+	void	*content;
+
+	dup = (t_list **)malloc(sizeof(t_list *));
+	if (dup == NULL)
+		return (NULL);
+	*dup = NULL;
+	temp = *lst;
+	while (temp != NULL)
+	{
+		content = ft_contdup(temp->content);
+		ft_lstadd_back(dup, ft_lstnew(content));
+		if (content == NULL || content != ft_lstlast(*dup)->content) 
+		{
+			ft_lstclear(dup, (void *)free);
+			free(content);
+			free(dup);
+			return (NULL);
+		}
+		temp = temp->next;
+	}
+	return (dup);
+}
+
+void *ft_contdup(void *content)
+{
+	int	*dup;
+
+	dup = malloc(sizeof(content));
+	if (dup == NULL)
+		return (NULL);
+	*dup = *(int *)content;
+	return (dup);
+}
+
+static void	ft_putlst_fd(t_list **lst, int fd)//delete at the end
+{	
+	ft_putstr_fd("putlst start:\n", fd);
+	ft_lstiter(*lst, (void *)ft_putcontent_fd);
+	ft_putstr_fd("putlst end;\n", fd);
+}
+
+static void	ft_putcontent_fd(void *content)//delete at the end
+{
+	ft_putnbr_fd(*(int *)content, 1);
+	ft_putchar_fd('\n', 1);
 }
