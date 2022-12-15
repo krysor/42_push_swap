0d3580bdde9aca7c7c45d5f76f5dafff6d2a0104
krysor <krysor901@gmail.com>
2022-12-15 15:06:03 +0100

    working on ft_getnb_ra functions

--- ft_putbest.c
+++ ft_putbest.c
@@ -2,6 +2,7 @@
 
 void	ft_putbest_free(t_list **ra, t_list **rra, t_list **rb, t_list **rrb);
 void	ft_get_nb_rb(t_list **rb);
+void	ft_get_nb_rrb(t_list **rrb);
 
 	//this is about ONE SINGLE MOVE
 int ft_putbest(t_list **a, t_list **b, char *result)
@@ -25,9 +26,17 @@ int ft_putbest(t_list **a, t_list **b, char *result)
 	
 	(void)a;
 	(void)result;
-	ft_get_nb_rb(rb);
-	printf("rb:________________\n");
+	printf("rb before get_nb_rb:________________\n");
 	ft_putlst_fd(rb, 1);
+	ft_get_nb_rb(rb);
+	printf("rb after get_nb_rb:________________\n");
+	ft_putlst_fd(rb, 1);
+
+	printf("rrb before get_nb_rrb:________________\n");
+	ft_putlst_fd(rrb, 1);
+	ft_get_nb_rrb(rrb);
+	printf("rrb after get_nb_rrb:________________\n");
+	ft_putlst_fd(rrb, 1);
 	
 	//calculate which should get put first
 		//calculate how many instructions of each type for each
@@ -41,8 +50,41 @@ int ft_putbest(t_list **a, t_list **b, char *result)
 		//append the instructions to the instructions strings
 			//possibly modify first if possible to optimize by 2 sides singel instruction moves
 	//execute the best one
+	ft_putbest_free(ra, rra, rb, rrb);
 	return (1);
 }
+
+void	ft_get_nb_ra(t_list **ra, t_list **a)
+{
+	int	i;
+	t_list *tempa;
+	t_list *tempra;
+
+	tempra = *ra;
+	while (tempra)
+	{
+		i = 0;
+		tempa = *a;
+		while (tempa)
+		{
+			if (ft_lstsize(*a) == 1)//special case if only 1 element in a
+			{
+				*(int *)(tempra->content) = i;
+				break ;
+			}
+			else if (tempra < *(int *)((*tempa)->content))
+			{
+				*(int *)(tempra->content) = i;
+				break ;
+			}
+			i++;
+			tempa = tempa->next;
+		}
+		tempra = tempra->next;
+	}
+}
+
+*(int *)(tempra->content) = i;
 
 void	ft_get_nb_rb(t_list **rb)
 {
@@ -51,10 +93,25 @@ void	ft_get_nb_rb(t_list **rb)
 
 	i = 0;
 	temp = *rb;
-	printf("halloo\n");
-	while (!temp)
+	while (temp)
 	{
 		*(int *)(temp->content) = i;
+		i++;
+		temp = temp->next;
+	}
+}
+
+void	ft_get_nb_rrb(t_list **rrb)
+{
+	t_list	*temp;
+	int		i;
+
+	i = ft_lstsize(*rrb);
+	temp = *rrb;
+	while (temp)
+	{
+		*(int *)(temp->content) = i;
+		i--;
 		temp = temp->next;
 	}
 }
