ba7f044561d3fea53d8c3104ec2e4441f9a64117
krysor <krysor901@gmail.com>
2022-12-17 10:38:17 +0100

    working on move executing function

--- ft_putbest.c
+++ ft_putbest.c
@@ -31,6 +31,21 @@ int ft_putbest(t_list **a, t_list **b, char *result)
 	ft_get_nb_rrb(rrb);
 	(void)b;
 	(void)result;
+	ft_getbestmoves(ra, rra, rb, rrb);
+	(void)ft_pickbest_move(ra, rra, rb, rrb);//call this inside move execute function
+	//split move execute function in rx and rrx (since ra and or rb)
+	printf("a: \n");
+	ft_putlst_fd(a, 1);
+	printf("b: \n");
+	ft_putlst_fd(b, 1);
+	printf("ra after\n");
+	ft_putlst_fd(ra, 1);
+	printf("rra after\n");
+	ft_putlst_fd(rra, 1);
+	printf("rb after\n");
+	ft_putlst_fd(rb, 1);
+	printf("rrb after\n");
+	ft_putlst_fd(rrb, 1);
 	//calculate which should get put first
 		//calculate how many instructions of each type for each
 			//get the information
@@ -45,6 +60,54 @@ int ft_putbest(t_list **a, t_list **b, char *result)
 	//execute the best one
 	ft_putbest_free(ra, rra, rb, rrb);
 	return (1);
+}
+
+//finish this one or think about how to get it
+int	ft_pickbest_move(t_list **ra, t_list **rra, t_list **rb, t_list **rrb)
+{
+	int	moves;
+
+	t_list *tempra;
+	t_list *temprra;
+	t_list *temprb;
+	t_list *temprrb;
+
+	tempra = *ra;
+	temprra = *rra;
+	temprb = *rb;
+	temprrb = *rrb;
+	moves = ft_get_nbmoves(tempra, temprra, temprb, temprrb);
+	while (tempra)
+	{
+		if (ft_get_nbmoves(tempra, temprra, temprb, temprrb) < moves)
+			moves = ft_getmoves(tempra, temprra, temprb, temprrb);
+		tempra = tempra->next;
+		temprra = temprra->next;
+		temprb = temprb->next;
+		temprrb = temprrb->next;
+	}
+	return (moves);
+}
+
+int	ft_get_nbmoves(t_list *ra, t_list *rra, t_list *rb, t_list *rrb)
+{
+	int	rarb;
+	int	rarrb;
+	int	rrarb;
+	int	rrarrb;
+	
+	rarb = ft_max(*(int *)(tempra->content), *(int *)(temprb->content));
+	rarrb = *(int *)(tempra->content) + *(int *)(temprrb->content);
+	rrarb = *(int *)(temprra->content) + *(int *)(temprb->content);
+	rrarrb = ft_max(*(int *)(temprra->content), *(int *)(temprrb->content));
+	if (rarb <= rarrb && rarb <= rrarb && rarb <= rrarrb)
+		return (rarb);
+	else if (rarrb <= rarb && rarrb <= rrarb && rarrb <= rrarrb)
+		return (rarrb);
+	else if (rrarb <= rarb && rrarb <= rarrb && rrarb <= rrarrb)
+		return (rrarb);
+	else if (rrarrb <= rarb && rrarrb <= rarrb && rrarrb <= rrarb)
+		return (rrarrb);
 }
 
 void	ft_putbest_free(t_list **ra, t_list **rra, t_list **rb, t_list **rrb)
