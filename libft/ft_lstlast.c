9e1cdebfefb598563d5605c65e74a097e57060ab
Krystian Kaczorowski <kkaczoro@mi-r6-p3.s19.be>
2022-12-20 13:06:46 +0100

    added header to ft_atopi.c

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkaczoro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: kkaczoro <kkaczoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 20:45:48 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/03/17 12:32:31 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/12/20 13:03:28 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 t_list	*ft_lstlast(t_list *lst)
 {
 	t_list	*l;
-	
+
 	if (lst == NULL)
 		return (NULL);
 	l = lst;
