eccf988cbc00622261832c893f7c2076148c48c2
Krystian Kaczorowski <kkaczoro@mi-r7-p1.s19.be>
2022-12-21 15:14:05 +0100

    changed function name to more logical in ft_putbest

--- ft_putbest.c
+++ ft_putbest.c
@@ -6,17 +6,17 @@
 /*   By: kkaczoro <kkaczoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 10:52:24 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/12/21 15:10:55 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/12/21 15:13:11 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static char	*ft_putbest_free_ret(t_list **ra, t_list **rra,
+static char	*ft_putbest_free_return(t_list **ra, t_list **rra,
 				t_list **rb, t_list **rrb);
 static void	ft_putbest_free(t_list **ra, t_list **rra,
 				t_list **rb, t_list **rrb);
-static void	ft_putbest_free_r(t_list **r);
+static void	ft_putbest_free_lst(t_list **lst);
 
 char	*ft_putbest(t_list **a, t_list **b, char *result)
 {
@@ -31,11 +31,11 @@ char	*ft_putbest(t_list **a, t_list **b, char *result)
 	rb = ft_get_nb_rb(ft_lstdup(b));
 	rrb = ft_get_nb_rrb(ft_lstdup(b));
 	if (ra == NULL || rra == NULL || rb == NULL || rrb == NULL)
-		return (ft_putbest_free_ret(ra, rra, rb, rrb));
+		return (ft_putbest_free_return(ra, rra, rb, rrb));
 	ft_getallbestmoves(ra, rra, rb, rrb);
 	moves = ft_getthebestmoves(ra, rra, rb, rrb);
 	if (!moves)
-		return (ft_putbest_free_ret(ra, rra, rb, rrb));
+		return (ft_putbest_free_return(ra, rra, rb, rrb));
 	result = ft_makethebestmoves(a, b, result, moves);
 	ft_putbest_free(ra, rra, rb, rrb);
 	free(moves);
@@ -45,25 +45,25 @@ char	*ft_putbest(t_list **a, t_list **b, char *result)
 static void	ft_putbest_free(t_list **ra, t_list **rra,
 				t_list **rb, t_list **rrb)
 {
-	ft_putbest_free_r(ra);
-	ft_putbest_free_r(rra);
-	ft_putbest_free_r(rb);
-	ft_putbest_free_r(rrb);
+	ft_putbest_free_lst(ra);
+	ft_putbest_free_lst(rra);
+	ft_putbest_free_lst(rb);
+	ft_putbest_free_lst(rrb);
 }
 
-static void	ft_putbest_free_r(t_list **r)
+static void	ft_putbest_free_lst(t_list **lst)
 {
 	int	p;
 
 	p = 1;
-	if (!r)
+	if (!lst)
 		p = 0;
-	ft_lstclear(r, (void *)free);
+	ft_lstclear(lst, (void *)free);
 	if (p)
-		free(r);
+		free(lst);
 }
 
-static char	*ft_putbest_free_ret(t_list **ra, t_list **rra,
+static char	*ft_putbest_free_return(t_list **ra, t_list **rra,
 				t_list **rb, t_list **rrb)
 {
 	ft_putbest_free(ra, rra, rb, rrb);
